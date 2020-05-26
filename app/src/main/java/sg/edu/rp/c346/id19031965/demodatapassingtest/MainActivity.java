2211e5c309733647161f6fd7c7ddab96f7e356c8
19031965 <19031965@myrp.edu.sg>
2020-05-26 22:51:30 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19031965/demodatapassingtest/MainActivity.java
@@ -0,0 +1,59 @@
+package sg.edu.rp.c346.id19031965.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Event handling Step 1
+    Button btnPassInteger;
+    Button btnPassCharacter;
+    TextView tvPassDouble;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Event handling Step 2
+        btnPassInteger = findViewById(R.id.buttonPassInteger);
+
+        //Event handling Step 3
+        btnPassInteger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+
+        btnPassCharacter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent2 = new Intent(MainActivity.this,ThirdActivity.class);
+                intent2.putExtra("character", 'g');
+                startActivity(intent2);
+            }
+
+        });
+
+        tvPassDouble.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent3 = new Intent(MainActivity.this,FourthActivity.class);
+                intent3.putExtra("double", 11.11);
+                startActivity(intent3);
+            }
+        });
+
+
+
+    }
+}
